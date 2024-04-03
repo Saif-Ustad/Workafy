04bc6fb009d8f7a7d7b23b4db51b20d7647bfa6d
Saif Ustad <saifustad12345@gmail.com>
2024-04-03 17:04:42 +0530

    projects page done

--- app/(dashboard)/(withoutSideMenu)/browse-project/page.jsx
+++ app/(dashboard)/(withoutSideMenu)/browse-project/page.jsx
@@ -1,9 +1,233 @@
-import React from 'react'
 
-const page = () => {
+"use client"
+
+
+import PaginationComponent from '@/components/DashboardPages/Pagination';
+import Image from 'next/image';
+
+import style from "@/style/DashboardPageStyles/Browse_projects.module.scss"
+
+
+
+// const projects = ["saif", "kaif", "aman", "sahil", "anjum", "suhana", "misba", "simran"];
+const itemsPerPage = 5;
+import projectsDetails from "@/constants/ProjectDetails";
+
+
+import { FaCaretDown } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
+import { FaCaretUp } from "react-icons/fa";
+
+import { GoArrowUpRight } from "react-icons/go";
+
+import { MdCheck } from "react-icons/md";
+
+import { useState } from 'react';
+
+import { Button, Menu, MenuItem } from '@mui/material';
+
+
+
+
+
+
+const Page = () => {
+
+  const [isFilterOpen, setIsFilterOpen] = useState(true);
+  const [isCategoryrOpen, setIsCategoryrOpen] = useState(false);
+  const [isBudgetOpen, setIsBudgetOpen] = useState(false);
+
+
+  const budgetItems = ['$0-$100', '$100-$500', '$500-$5000', '$5000-$10000', '$10000 and above']
+
+  const [selectedBudget, setSelectedBudget] = useState('');
+
+  const handleBudgetSelect = (budget) => {
+    setSelectedBudget(budget);
+  }
+
+
+
+  const categoryItems = ['IT, Web and Mobile ', 'Design & Multimedia ', 'Writing & Translation', 'Admin Support', 'Engineering & Architecture', 'Marketing & Sales', 'Management and Finance', 'Legal']; // Example items
+
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+
+
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+
   return (
-    <div>browse projects</div>
-  )
-}
+    // <div>
+    //   <h1>Projects</h1>
+    //   <PaginationComponent projects={projects} itemsPerPage={itemsPerPage} />
+    // </div>
+    <div className='mx-[15px]'>
 
-export default page
+      <div className="relative rounded-[15px]  overflow-hidden bg-cover bg-center h-[300px] " style={{ backgroundImage: "url('/Images/Banner_background.jpg')" }}>
+        <div className="h-full container  p-[20px] mx-auto flex flex-col justify-center">
+          <h1 className="text-[25px] md:text-[30px] font-bold text-headings  mb-[15px]">Find Work</h1>
+          <p className="text-[13px] md:text-[15px] text-headings ">Anything For You: Unlock your next opportunity, find work tailored to your skills and passion!</p>
+          <div className='absolute hidden xl:block right-[100px] top-[20px]'><Image src={"/Images/banner_Curly_img.png"} alt='photo' width={400} height={400} className={style.bounceY} /></div>
+        </div>
+      </div>
+
+
+    
+      <div className='container   sm:p-[20px]  mx-auto flex flex-col xl:flex-row mt-[20px] gap-[20px] '>
+        <div className='xl:w-[25%]   '>
+
+            {/* //filter */}
+          <div>
+            <div className='flex justify-between cursor-pointer' onClick={() => setIsFilterOpen(!isFilterOpen)}>
+              <h3 className="text-[16px] text-headings md:text-[18px] font-medium ">Filter</h3>
+              <span>{isFilterOpen ? <FaCaretUp /> : <FaCaretDown />} </span>
+            </div>
+            <div className={`transition-max-h duration-500 ease-in-out max-h-0 overflow-hidden  mt-[20px] ${isFilterOpen ? 'max-h-[200px]' : ''} relative text-customDarkGreen mb-[20px]`}>
+              <input placeholder='Keywords' className='w-full border border-gray-200   py-[7px] px-[20px] rounded-[8px]' />
+              <span className='absolute right-3 top-1/2 transform -translate-y-1/2 text-[18px] '><IoSearchOutline /></span>
+            </div>
+          </div>
+
+
+          <hr className='mb-[20px]' />
+
+
+
+          {/* //category */}
+          <div>
+            <div className='flex justify-between cursor-pointer' onClick={() => setIsCategoryrOpen(!isCategoryrOpen)}>
+              <h3 className="text-[16px] text-headings md:text-[18px] font-medium ">Category</h3>
+              <span>{isCategoryrOpen ? <FaCaretUp /> : <FaCaretDown />} </span>
+            </div>
+            <ul className={`transition-max-h duration-500 ease-in-out max-h-0 overflow-hidden  mt-[20px] ${isCategoryrOpen ? 'max-h-[400px]' : ''}  text-customDarkGreen mb-[20px] flex flex-col gap-[15px]`}>
+              {categoryItems.map((category, id) => (
+                <li key={id} onClick={() => handleCategorySelect(category)} className='flex items-center gap-[10px]'>
+                  <input type="checkbox" checked={selectedCategory === category} readOnly />
+                  <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>{category}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <hr className='mb-[20px] ' />
+
+
+
+          {/* //budget */}
+          <div>
+            <div className='flex justify-between cursor-pointer' onClick={() => setIsBudgetOpen(!isBudgetOpen)}>
+              <h3 className="text-[16px] text-headings md:text-[18px] font-medium  ">Budget</h3>
+              <span>{isBudgetOpen ? <FaCaretUp /> : <FaCaretDown />} </span>
+            </div>
+            <ul className={`transition-max-h duration-500 ease-in-out max-h-0 overflow-hidden mt-[20px] text-customDarkGreen mb-[20px] flex flex-col gap-[15px] ${isBudgetOpen ? 'max-h-[200px]' : ''}`}>
+
+              {budgetItems.map((budget, id) => (
+                <li key={id} onClick={() => handleBudgetSelect(budget)} className='flex items-center gap-[10px]'>
+                  <input type="checkbox" checked={selectedBudget === budget} readOnly />
+                  <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>{budget}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='flex flex-col gap-[10px] items-center border border-gray-200 rounded-[15px] shadow-sm p-[20px]'>
+            <Image src={"/Images/badge.png"} alt='badge' className='mb-[10px]' width={100} height={100} />
+            <h2 className='text-[16px] text-customDarkGreen font-medium'> Get projects 20X faster!</h2>
+            <div className="py-[15px] bg-[#f1fcfa]  w-full rounded-full  font-semibold text-[15px] flex gap-2 items-center justify-center text-customGreen cursor-pointer hover:bg-customGreen hover:text-white">Go Premium<span className="text-[20px]"><GoArrowUpRight /></span></div>
+
+            <hr className='w-full my-[10px]' />
+
+            <ul className='flex flex-col gap-[20px]'>
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Top Listing in Client Searches</span>
+              </li>
+
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Direct Recommendations</span>
+              </li>
+
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Apply on 200 Projects/month</span>
+              </li>
+
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Workafy Premium Badge</span>
+              </li>
+
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Work in All Skill Categories</span>
+              </li>
+
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Weekly Profile Reviews</span>
+              </li>
+
+              <li className='flex items-center gap-[14px]'>
+                <span className='bg-[#5BBB7B26] rounded-full p-1 text-[12px] text-[#5bbb7b]'><MdCheck /></span>
+                <span className='text-customDarkGreen text-[15px] sm:text-[16px]'>Workafy Priority Support</span>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+
+        <div className='xl:w-[75%]'>
+          <div className='flex justify-between items-center mb-[20px]'>
+            <h2 className='text-customDarkGreen text-[15px] sm:text-[16px] font-medium'>Available Projects : {projectsDetails.length}+</h2>
+            
+            <div>
+              <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                variant="contained"
+              >
+                Sort by
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>New Arrival (Latest)</MenuItem>
+              </Menu>
+            </div>
+            
+          </div>
+
+          <div>
+            <PaginationComponent projects={projectsDetails} itemsPerPage={itemsPerPage} />
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Page;
---
+++ components/DashboardPages/Pagination.jsx
@@ -0,0 +1,110 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
+
+import React, { useEffect, useState } from 'react';
+import { Pagination } from '@mui/material';
+
+import { CiCalendarDate } from "react-icons/ci";
+import { PiNoteLight } from "react-icons/pi";
+import { CiLocationOn } from "react-icons/ci";
+import { GoArrowUpRight } from "react-icons/go";
+import { LuBookmark } from "react-icons/lu";
+
+
+const PaginationComponent = ({ projects, itemsPerPage }) => {
+
+  const router = useRouter();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(projects.length / itemsPerPage);
+
+  const searchParams = useSearchParams();
+
+  
+
+// Set initial currentPage to 1 (not using router.query)
+  useEffect(() => {
+    const page = searchParams.get("page");
+    if (page && !isNaN(page)) {
+      setCurrentPage(parseInt(page));
+    }
+  }, [router.query]);
+    
+
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+    router.push(`/browse-project?page=${value}`);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const projectsToShow = projects.slice(startIndex, endIndex);
+
+  return (
+    <div>
+      {projectsToShow.map((project, index) => (
+        <div key={index} className="my-[25px] p-[25px] sm:p-[40px] border rounded-lg flex flex-col md:flex-row gap-[25px] md:gap-0 ">
+
+          <div className='md:w-[68%] flex flex-col justify-center gap-[15px] md:pr-[25px] md:border-r border-gray-200'>
+
+            <h1 className="text-[18px] text-headings md:text-[19px] font-medium  ">{project.title}</h1>
+
+            <div className='flex flex-wrap '>
+              <div className="flex items-center gap-[10px] border-r border-gray-200 px-[15px]">
+                <span className='text-[18px]'><CiCalendarDate /></span>
+                <p className='text-[13px] md:text-[15px]  text-[#6b7177] '>{project.date}</p>
+              </div>
+
+              <div className="flex items-center gap-[10px] border-r border-gray-200 px-[15px]">
+                <span className='text-[18px]'><PiNoteLight /></span>
+                <p className='text-[13px] md:text-[15px]  text-[#6b7177] '>{project.category}</p>
+              </div>
+
+              <div className="flex items-center gap-[10px] px-[15px]">
+                <span className='text-[18px]'><PiNoteLight /></span>
+                <p className='text-[13px] md:text-[15px]  text-[#6b7177] '>{project.subCategory}</p>
+              </div>
+            </div>
+
+            <p className='text-[13px] md:text-[15px]  text-headings leading-[1.75] text-justify'>{project.description}</p>
+
+          </div>
+
+
+          <div className='md:w-[32%] md:pl-[25px]'>
+            <div className='flex flex-col gap-[5px] md:items-end mb-[15px]'>
+              <h1 className="text-[16px] text-headings md:text-[17px] font-medium  ">Budget : {project.budget}</h1>
+              <h2 className="text-[15px] text-headings md:text-[16px] font-medium  ">INR: {project.budgetINR}</h2>
+              <div className='flex items-center gap-[6px]'>
+                <span><CiLocationOn /></span>
+                <h3 className='text-[13px] md:text-[14px]  text-headings'>{project.location}</h3>
+              </div>
+            </div>
+
+            <div className="py-[15px] bg-[#f1fcfa]  w-full rounded-md font-semibold text-[15px] flex gap-2 items-center justify-center text-customGreen cursor-pointer hover:bg-customGreen hover:text-white mb-[10px]">Apply<span className="text-[20px]"><GoArrowUpRight /></span></div>
+            <div className="py-[15px] bg-[#f1fcfa]  w-full rounded-md  font-semibold text-[15px] flex gap-2 items-center justify-center text-customGreen cursor-pointer hover:bg-customGreen hover:text-white"><span className="text-[20px]"><LuBookmark /></span>Save for later</div>
+
+          </div>
+
+        </div>
+      ))}
+
+      <div className="mt-4 flex flex-col items-center gap-[15px]">
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+          variant="outlined"
+          shape="rounded"
+        />
+         <div className='text-[12px] md:text-[14px]  text-headings'>{`${startIndex} - ${endIndex} of ${projects.length}+ projects available`}</div>
+      </div>
+    </div>
+
+  );
+};
+
+export default PaginationComponent;
