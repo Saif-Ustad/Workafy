25055d2a3cdd2e420d4f874b8944de00269b4c53
Saif Ustad <saifustad12345@gmail.com>
2024-04-04 06:46:45 +0530

    retry

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../style/globals.css"
 
-
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +33,9 @@ export default function RootLayout({
     <ClerkProvider>
       <GlobalProvider>
         <html lang="en">
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
           <body className={inter.className}>
             <ClerkLoading>
               <Loading />
